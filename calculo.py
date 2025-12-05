2f0dacd3b69ece6ca11ba2bd1a852aa0bbd28291
RangS <rangga19sj@gmail.com>
2025-12-05 21:15:23 +0700

    Change Ruby script reference from calculator.rb to process.rb

--- calculo.py
+++ calculo.py
@@ -7,7 +7,7 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 
-RUBY_SCRIPT = 'calculator.rb'
+RUBY_SCRIPT = 'process.rb'
 
 class Calculator(QWidget):
     def __init__(self):
