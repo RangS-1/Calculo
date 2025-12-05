ca30f8fb3076a9506123dbc172e9e7e2d4ceef53
RangS <rangga19sj@gmail.com>
2025-12-05 21:13:06 +0700

    Add basic calculator functionality in process.rb
    Implement a basic calculator that supports addition, subtraction, multiplication, exponentiation, modulus, and division. Includes error handling for invalid input and division by zero.

---
+++ process.rb
@@ -0,0 +1,43 @@
+if ARGV.length != 3
+  STDERR.puts "Usage: ruby calculator.rb <operator> <operand1> <operand2>"
+  exit 2
+end
+
+op = ARGV[0]
+begin
+  a = Float(ARGV[1])
+  b = Float(ARGV[2])
+rescue => e
+  STDERR.puts "Invalid number"
+  exit 3
+end
+
+result = nil
+case op
+when '+'
+  result = a + b
+when '-'
+  result = a - b
+when 'x'
+  result = a * b
+when '^'
+  result = a ** b
+when '%'
+  result = a % b
+when '/'
+  if b == 0.0
+    STDERR.puts "Nah"
+    exit 4
+  end
+  result = a / b
+else
+  STDERR.puts "Not Supported"
+  exit 5
+end
+
+# Hapus trailing zeros jika merupakan bilangan bulat
+if result == result.to_i
+  puts result.to_i
+else
+  puts result
+end
